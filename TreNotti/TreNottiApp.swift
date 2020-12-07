d1a984474480a5109b19d67c1408bc6d3e032829
Kentaro <kentaro@beah.jp>
2020-12-08 02:34:31 +0900

    Initial Commit

---
+++ TreNotti/TreNottiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreNottiApp.swift
+//  TreNotti
+//
+//  Created by Kentaro Abe on 2020/12/08.
+//
+
+import SwiftUI
+
+@main
+struct TreNottiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
